69777a2814a1c370ce15e2bb096268d5fd4f6bcc
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-09-29 11:44:05 -0500

    a script that will return a table of articles per country from a medline file

---
+++ articles_by_country.py
@@ -0,0 +1,53 @@
+import argparse
+from Bio import Medline
+import time, datetime
+from pprint import pprint
+
+parser = argparse.ArgumentParser(description='grab adscriptions from medline')
+parser.add_argument('--citations', type=argparse.FileType('r'), required=True)
+
+args    = parser.parse_args()
+
+
+countries = [n.strip() for n in open('iso_3166.txt').readlines()]
+
+count = {}
+
+adscriptions = set()
+records = Medline.parse( args.citations )
+for r in records:
+    
+    # evenly format dates
+    if 'CRDT' in r.keys():
+        conv = time.strptime( r['CRDT'][0], "%Y/%m/%d %H:%M" )
+        r['CRDT'] = datetime.datetime(*conv[:6]) # date created
+    if 'DCOM' in r.keys():
+        conv = time.strptime( r['DCOM'], "%Y%m%d" )
+        r['DCOM'] = datetime.datetime(*conv[:6]) # date completed
+    if 'LR' in r.keys():
+        conv = time.strptime( r['LR'], "%Y%m%d" )
+        r['LR'] = datetime.datetime(*conv[:6]) # date revised
+    if 'DEP' in r.keys():
+        conv = time.strptime( r['DEP'], "%Y%m%d" )
+        r['DEP'] = datetime.datetime(*conv[:6]) # date of electronic publication
+
+    # let PubMed handle keys
+    r['_id'] = int(r['PMID'])
+
+    if 'AD' in r:
+#        print r['PMID']
+#        pprint(r['AD'])
+        ad = [n.replace('.','').replace(',','').replace(':','').replace(';','') for n in r['AD'].split()]
+        for country in countries:
+            c = country
+            if c in ad:
+
+                if c in count:
+                    count[c] += 1
+                else:
+                    count[c] = 1
+
+
+for c in count:
+    print "%s,%s" % (c, count[c])
+
